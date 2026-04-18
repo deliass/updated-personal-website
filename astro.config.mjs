37cc3f4b42bcdafd1b85681c49d4425ff4f2857f
Nélia Figuerola <neliafiguerola@gmail.com>
2026-04-18 18:38:15 +0200

    github

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nelia.statichost.page/',
+	site: 'https://deliass.github.io',
 	base: '/updated-personal-website',
 });
